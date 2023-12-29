51ad17779f1d7821a503ce68353c2aedd3f5ca5f
ERFAN <erfan.shokr300@gmail.com>
2023-12-29 19:14:06 +0330

    first commit

---
+++ install-vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>hello vue</h1>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
